50ca04cf84d49cd1c9139627d515e3cb41a78727
Bharath <potu.bharath513@gmail.com>
2019-03-15 16:46:42 +0530

    commit

--- app/src/main/java/com/bharath/twoactivities/MainActivity.java
+++ app/src/main/java/com/bharath/twoactivities/MainActivity.java
@@ -9,11 +9,14 @@ import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        tv=findViewById(R.id.tv1);
+        tv.setText("User1");
     }
     public void sendmessage(View view) {
         EditText msg=(EditText)findViewById(R.id.msg);
